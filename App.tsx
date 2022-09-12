282ba98ab551104f5ccd0340ae89caaf21bbdb4c
Sofyanne Badir <s.badir@outlook.com>
2022-09-12 14:43:58 +0200

    :hammer: StepZen connexion, tabNavigator added with search input

--- App.tsx
+++ App.tsx
@@ -1,25 +1,17 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
 import {TailwindProvider} from 'tailwind-rn';
 import utilities from './tailwind.json';
-import CustomersScreen from "./screens/CustomersScreen";
+import {NavigationContainer} from "@react-navigation/native";
+import RootNavigator from "./navigator/RootNavigator";
 
 
 export default function App() {
     // @ts-ignore - TailwindProvider is missing a type definition
     return (
         <TailwindProvider utilities={utilities}>
-            <CustomersScreen />
+            <NavigationContainer>
+                <RootNavigator />
+            </NavigationContainer>
         </TailwindProvider>
 
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,18 +1,38 @@
-import {View, Text, SafeAreaView} from "react-native";
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {View, Text, SafeAreaView, ScrollView, ActivityIndicator} from "react-native";
+import React, {useLayoutEffect, useState} from 'react';
+import {CompositeNavigationProp, NavigationContainer, useNavigation} from '@react-navigation/native';
 import {useTailwind} from 'tailwind-rn';
+import {BottomTabNavigationProp} from "@react-navigation/bottom-tabs";
+import {TabStackParamList} from "../navigator/TabNavigator";
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+import {RootStackParamList} from "../navigator/RootNavigator";
+import {Image, Input} from "@rneui/themed";
 
+export type CustomerScreenNavigationProps = CompositeNavigationProp<BottomTabNavigationProp<TabStackParamList, "Customers">,
+    NativeStackNavigationProp<RootStackParamList>>;
 
 const CustomersScreen = () => {
     const tw = useTailwind();
+    const navigation = useNavigation<CustomerScreenNavigationProps>();
+    const [input, setInput] = useState<string>("");
+
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        })
+    }, [])
 
     return (
-        <SafeAreaView>
-            <NavigationContainer>
-                <Text style={tw("text-blue-500")}>CustomersScreens</Text>
-            </NavigationContainer>
-        </SafeAreaView>
+        <ScrollView style={{ backgroundColor: "#59C1CC"}}>
+            <Image source={{ uri: "http://links.papareact.com/3jc"}}
+            containerStyle={tw("w-full h-64")}
+            PlaceholderContent={<ActivityIndicator />}
+            />
+
+            <Input placeholder="Search by Customer" value={input} onChangeText={setInput} containerStyle={tw("bg-white pt-5 pb-0 px-10")}/>
+
+        </ScrollView>
     );
 };
 
