d9585023ce6379d7f96b201cefdba0f7f7b5f113
Sofyanne Badir <s.badir@outlook.com>
2022-09-13 14:31:53 +0200

    :hammer: Apollo client integration

--- App.tsx
+++ App.tsx
@@ -2,15 +2,23 @@ import {TailwindProvider} from 'tailwind-rn';
 import utilities from './tailwind.json';
 import {NavigationContainer} from "@react-navigation/native";
 import RootNavigator from "./navigator/RootNavigator";
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
 
+
+const client = new ApolloClient({
+    uri: 'http://localhost:5001/api/deliverapp',
+    cache: new InMemoryCache(),
+});
 
 export default function App() {
     // @ts-ignore - TailwindProvider is missing a type definition
     return (
         <TailwindProvider utilities={utilities}>
-            <NavigationContainer>
-                <RootNavigator />
-            </NavigationContainer>
+            <ApolloProvider client={client} >
+                <NavigationContainer>
+                    <RootNavigator/>
+                </NavigationContainer>
+            </ApolloProvider>
         </TailwindProvider>
 
     );
