a5ba6e80e5409f73956f48ee3482ad9fe30d8701
Sofyanne Badir <s.badir@outlook.com>
2022-09-13 14:06:44 +0200

    :label:Added Item, OrderResponse, CustomerResponse, Order types

--- typing.d.ts
+++ typing.d.ts
@@ -6,4 +6,38 @@ type Customer = {
 type CustomerList = {
     name: ID;
     value: Customer;
-}
+};
+
+type TrackingItem = {
+    customer_id: ID;
+    customer: Customer;
+    items: Item[];
+};
+
+type Item = {
+  item_id: ID;
+  name: string;
+  price: number;
+  quantity: number;
+};
+
+type OrderResponse = {
+    value: Order;
+};
+
+type CustomerResponse = {
+    name: ID;
+    value: Customer;
+};
+
+type Order = {
+  carrier: string;
+  createdAt: string;
+  shippingCost: number;
+  trackingId: string;
+  trackingItems: TrackingItem;
+  Lat: number;
+  Lng: number;
+  Address: string;
+  City: string;
+};
