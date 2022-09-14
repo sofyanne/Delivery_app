66fd3047528effd1ddd91892f707e537b6270680
Sofyanne Badir <s.badir@outlook.com>
2022-09-14 19:00:41 +0200

    :hammer: navigation to order added

--- components/OrderCard.tsx
+++ components/OrderCard.tsx
@@ -2,16 +2,26 @@ import {Text, TouchableOpacity, View} from "react-native";
 import React from 'react';
 import {Card, Icon} from "@rneui/themed";
 import {useTailwind} from "tailwind-rn";
+import {CompositeNavigationProp, useNavigation} from "@react-navigation/native";
+import {BottomTabNavigationProp} from "@react-navigation/bottom-tabs";
+import {TabStackParamList} from "../navigator/TabNavigator";
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+import {RootStackParamList} from "../navigator/RootNavigator";
 
+
+export type OrderScreenNavigationProp = CompositeNavigationProp<
+    BottomTabNavigationProp<TabStackParamList, "Orders">,
+    NativeStackNavigationProp<RootStackParamList>>;
 
 type Props = {
     item: Order
 }
 const OrderCard = ({item}: Props) => {
     const tw = useTailwind();
+    const navigation = useNavigation<OrderScreenNavigationProp>();
 
     return (
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Order", {order: item})}>
             <Card containerStyle={tw("p-5 rounded-lg")}>
                 <View style={tw("flex-row justify-between items-center")}>
                     <View>
