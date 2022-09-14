b8a616a85aa15b8a717f4bf24acf1d64ee8bc453
Sofyanne Badir <s.badir@outlook.com>
2022-09-14 09:55:39 +0200

    :hammer: Modal screen integration

--- navigator/RootNavigator.tsx
+++ navigator/RootNavigator.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import TabNavigator from "./TabNavigator";
+import ModalScreen from "../screens/ModalScreen";
 
 export type RootStackParamList = {
     Main: undefined;
@@ -8,13 +9,19 @@ export type RootStackParamList = {
     Order: { order: any }
 };
 
-const RootStack = createNativeStackNavigator();
+const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 const RootNavigator = () => {
     return (
         <RootStack.Navigator>
             <RootStack.Group>
                 <RootStack.Screen name="Main" component={TabNavigator}/>
+            </RootStack.Group>
+
+            <RootStack.Group screenOptions={{
+                presentation: "modal",
+            }}>
+                <RootStack.Screen options={{ headerShown: false}} name="MyModal" component={ModalScreen}/>
             </RootStack.Group>
         </RootStack.Navigator>
     );
